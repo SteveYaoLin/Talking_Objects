7bbf2bc4ea5b605a723ad53c400a72305e35ad71
SteveYaoLin <yaolin0616@163.com>
2024-06-06 16:17:40 +0800

    uvm_analysis_port使用方法，1.声明变量并定义数据类型，2.building_phase里例化roll_ap = new("roll_ap",this)。3.在run_phase里用write方法把数据the_roll写到uvm_analysis_port里。roll_ap.write(the_roll)。同时在dice_test里connect_phase。于是，在三个继承自uvm_subscriber的class里的参数他，就是要传递的数。各自使用

--- dice_roller.svh
+++ dice_roller.svh
@@ -16,23 +16,39 @@
 class dice_roller extends uvm_component;
    `uvm_component_utils(dice_roller);
 
+   uvm_analysis_port #(int) roll_ap;
+
+   function void build_phase (uvm_phase phase);
+      
+      roll_ap = new("roll_ap",this);
+      
+   endfunction : build_phase
+   
    rand byte die1;
    rand byte die2;
-
+   
    constraint d6 { die1 >= 1; die1 <= 6; 
                    die2 >= 1; die2 <= 6; }
+
+
+   task run_phase(uvm_phase phase);
+      int the_roll;
+      phase.raise_objection(this);
+      void'(randomize());
+      repeat (20) begin
+         void'(randomize());
+         the_roll = die1 + die2;
+         roll_ap.write(the_roll);
+      end
+      phase.drop_objection(this);
+   endtask : run_phase
    
+
+
    function new(string name, uvm_component parent);
       super.new(name,parent);
    endfunction : new
-
-   function byte two_dice();
-      byte the_roll;
-      void'(randomize());
-      the_roll = die1 + die2;
-      return the_roll;
-   endfunction : two_dice
-
+   
 endclass : dice_roller
 
 
